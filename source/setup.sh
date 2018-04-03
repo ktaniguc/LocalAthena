b869b666d3ae876dee6a6cb9b910d3e86fd63daf
Yuhi Fukuhara <yuhi.fukuhara@cern.ch>
2018-04-03 16:46:13 +0900

    Update latest asetup version in setup.sh

--- source/setup.sh
+++ source/setup.sh
@@ -10,7 +10,8 @@ setupATLAS
 #asetup r2017-04-12T2135,AthenaP1,21.0
 #asetup r2018-04-12T2135,AthenaP1,21.0
 #asetup r2018-03-13T2125,Athena,21.0
-asetup r2018-03-09T2222,Athena,21.0
+#asetup r2018-03-09T2222,Athena,21.0
+asetup r2018-03-29T2156,Athena,21.3
 #asetup 21.0,Athena,r26
 #asetup 21.0,Athena,r31
 #asetup AtlasOffline,21.0.20,slc6,gcc62,64
