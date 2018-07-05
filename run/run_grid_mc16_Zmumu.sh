b471e3a5adfdcda5bf495e51630d9369f764a854
Yuhi Fukuhara <yuhi.fukuhara@cern.ch>
2018-07-05 11:38:20 +0900

    Fix run_grid_mc16_zmumu

--- run/run_grid_mc16_Zmumu.sh
+++ run/run_grid_mc16_Zmumu.sh
@@ -28,13 +28,15 @@ cp ../*.sh ./
 echo ${0} 2>&1 | tee -a jediTaskID.info
 echo ""
 
+COMMAND="pathena --trf \"Reco_tf.py --outputAODFile %OUT.AOD.pool.root --inputRDOFile %IN\" --inDS ${INPUT_RDO} --outDS ${OUTPUT_AOD} --cmtConfig ${CMTCONFIG} --nFilesPerJob 5 --nEventsPerFile 500 --nEventsPerJob 500 --destSE TOKYO-LCG2_SCRATCHDISK"
+
 echo "COMMAND:"
-echo "pathena --trf \"Reco_tf.py --maxEvents %MAXEVENTS --outputAODFile %OUT.AOD.pool.root --inputRDOFile %IN\" --inDS ${INPUT_RDO} --outDS ${OUTPUT_AOD} --cmtConfig ${CMTCONFIG} --nFilesPerJob 2 --destSE TOKYO-LCG2_SCRATCHDISK" 2>&1 | tee -a jediTaskID.info
+echo $COMMAND
 
 echo "" | tee -a jediTaskID.info
 echo "LOG: " | tee -a jediTaskID.info
 
-pathena --trf "Reco_tf.py --maxEvents %MAXEVENTS --outputAODFile %OUT.AOD.pool.root --inputRDOFile %IN" --inDS ${INPUT_RDO} --outDS ${OUTPUT_AOD} --cmtConfig ${CMTCONFIG} --nFilesPerJob 5 --nEventsPerFile 500 --nEventsPerJob 500 --site "ANALY_CERN_SLC6,ANALY_BNL_LONG" --destSE TOKYO-LCG2_SCRATCHDISK 2>&1 | tee -a jediTaskID.info
+eval $COMMAND 2>&1 | tee -a jediTaskID.info
 
 JEDITASKID=$(sed -n '/new jediTaskID=/s/INFO : succeeded. new jediTaskID=//p' jediTaskID.info)
 #sed -n '/new jediTaskID/p' tmp_test.txt
