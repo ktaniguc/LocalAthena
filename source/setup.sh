7ef4a01ebbeb98a09121e0a90e169875f29d946d
Yuhi Fukuhara <yuhi.fukuhara@cern.ch>
2018-07-12 17:24:43 +0900

    Change setup

--- source/setup.sh
+++ source/setup.sh
@@ -11,7 +11,13 @@ setupATLAS
 #asetup r2018-04-12T2135,AthenaP1,21.0
 #asetup r2018-03-13T2125,Athena,21.0
 #asetup r2018-03-09T2222,Athena,21.0
-asetup r2018-03-29T2156,Athena,21.3
+#asetup r2018-03-29T2156,Athena,21.3
+
+# For user.yfukuhar.mc16_13TeV.361107.PowhegPythia8EvtGen_AZNLOCTEQ6L1_Zmumu.simul.HITS.e3601_s3126
+#asetup Athena,21.0.32,here
+
+# For mc16_13TeV.424108.Pythia8B_A14_CTEQ6L1_Jpsimu6.simul.HITS.e5441_e5984_s3126
+asetup AtlasOffline,21.0.15,here
 #asetup 21.0,Athena,r26
 #asetup 21.0,Athena,r31
 #asetup AtlasOffline,21.0.20,slc6,gcc62,64
